82674ddbb1eb00de239e7f31487cbf46ba59cab2
Élvio <ak4iwa@gmail.com>
2015-07-31 15:23:33 +0100

    neverminds this

--- 74100/74100/BatchFiles/test of tests.bat
+++ 74100/74100/BatchFiles/test of tests.bat
@@ -3,16 +3,7 @@
 
 :MainFb
 
-start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag timeout x100 STEALING MATRIX" 8 Networks/bara.1000.z4.links bara.z4 1 1 0 0 8
-timeout /T 830 /NOBREAK
-
-start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag timeout x100 STEALING MATRIX" 8 Networks/bara.500.z8.links bara.z8 1 1 0 0 6
-timeout /T 630 /NOBREAK
-
-start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag timeout x100 STEALING MATRIX" 8 Networks/bara.500.z6.links bara.z6 1 1 0 0 6
-timeout /T 630 /NOBREAK
-
-start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag timeout x100 STEALING MATRIX" 8 Networks/bara.500.z4.links bara.z4 1 1 0 0 5
-timeout /T 530 /NOBREAK
+start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag miniB timeout x100" 2 Networks/bara.500.z4.links bara.z4 1 1 0 0 2
+timeout /T 230 /NOBREAK
 
 exit
