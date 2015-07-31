31a5440cb873cf817976d4d910d8528b6566d760
Élvio <elviopabreu@gmail.com>
2015-07-31 11:20:41 +0100

    Revert afc6fa7..a3e9cd0
    This rolls back to commit afc6fa7aa4749f26542bf844709dd4295ac59ffe.

--- 74100/74100/BatchFiles/testing/makedir - Copy - Copy.bat
+++ 74100/74100/BatchFiles/testing/makedir - Copy - Copy.bat
@@ -3,19 +3,18 @@
 
 
 
-for /f "tokens=1,2,3,4,5,6,7,8,9,10* delims=_" %%a in ('dir /b Results\*.txt') do (
+for /f "tokens=1,2,3,4,5,6,7* delims=_" %%a in ('dir /b Results\*.txt') do (
 
 echo.%%b | findstr /C:"bara" 1>nul
 
 if errorlevel 1 (
-  set folderName=%%a__%%b_%%c_%%d__%%e__%%f__%%g__%%h__%%i
+  echo %%a__%%b_%%c_%%d__%%e__%%f
 ) ELSE (
-  set folderName=%%a__%%b_%%c__%%d__%%e__%%f__%%g__%%h
+  echo %%a__%%b_%%c__%%d__%%e
 )
 goto break
 )
 
 :break
-echo %folderName%
 pause
 exit
