a4631a83c9a7e549b3fa4d965c6fec5b4a284021
Élvio <ak4iwa@gmail.com>
2015-08-06 18:05:23 +0100

    nvm this

--- 74100/74100/BatchFiles/test of tests.bat
+++ 74100/74100/BatchFiles/test of tests.bat
@@ -3,7 +3,7 @@
 
 :MainFb
 
-start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag miniB timeout x100" 2 Networks/bara.500.z4.links bara.z4 1 1 0 0 2
+start "" "tag networkPath networkType tauTag tauStrat noiseStrat noiseTag miniB timeout x100" 8 Networks/bara.500.z4.links bara.z4 100 1 0 0 2
 timeout /T 230 /NOBREAK
 
 exit
