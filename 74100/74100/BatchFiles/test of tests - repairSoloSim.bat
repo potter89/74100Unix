c65b40b3cd64c36a1ab92f6c1d133043a5e6fbd6
Élvio-Windows <ak4iwa@gmail.com>
2015-09-05 10:54:46 +0100

    Revert "batchfile small fix"
    This reverts commit a14440cb3202d89be79f947990a732fefc81b2f6.

--- 74100/74100/BatchFiles/test of tests - repairSoloSim.bat
+++
@@ -1,8 +0,0 @@
-@setlocal
-@echo off
-
-
-start "sim2" "soloSim" 8 Networks/bara.500.z4.links bara.z4 0 1 4.5 0 0 1.25
-start "sim2" "soloSim" 8 Networks/bara.500.z4.links bara.z4 0 1 4.5 0 0 1.75
-
-exit
